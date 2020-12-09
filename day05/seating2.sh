c0eb2b912b48457dca39650ec16fc0d2ad2ae78b
David Rasch <rasch@raschnet.com>
2020-12-09 09:36:21 -0700

    day05 - clean up tempfiles

--- day05/seating2.sh
+++ day05/seating2.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-TMP=$(tempfile)
+TMP=$(tempfile) || exit
+trap "rm -f -- '$TMP'" EXIT
 
 cat input | tr FBLR 0101 | xargs -i  bash -c 'echo "$((2#{}))"'  | sort -n > $TMP
 HIGH=$(tail -1 $TMP)
 LOW=$(head -1 $TMP)
 
-TMP2=$(tempfile)
+TMP2=$(tempfile) || exit
+trap "rm -f -- '$TMP2'" EXIT
 echo $LOW $HIGH
 seq $LOW $HIGH > $TMP2
 
@@ -14,3 +16,7 @@ echo $TMP
 echo $TMP2
 
 diff $TMP $TMP2
+
+rm -f -- "$TMP" "$TMP2"
+trap - EXIT
+exit
