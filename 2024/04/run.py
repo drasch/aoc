14ea76293711c7e5d20aa996a9297adddf82c1ab
David Rasch <rasch@raschnet.com>
2024-12-04 08:33:41 -0500

    day 4

---
+++ 2024/04/run.py
@@ -0,0 +1,53 @@
+import itertools
+
+with open("input") as file:
+    lines = [line.strip() for line in file.readlines()]
+
+slines = [
+    line.strip()
+    for line in """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX""".split()
+]
+
+
+permutations = (-1, 0, 1)
+kernels = [(x, y) for x in permutations for y in permutations if x != 0 or y != 0]
+
+match = "XMAS"
+
+
+def get_string(lines, pos, kernel, match):
+    try:
+        xend = pos[0] + kernel[0] * (len(match) - 1)
+        yend = pos[1] + kernel[1] * (len(match) - 1)
+        if xend < 0 or xend > len(lines[0]) or yend < 0 or yend > len(lines):
+            return False
+
+        return match == "".join(
+            [
+                lines[pos[1] + (step * kernel[1])][pos[0] + (step * kernel[0])]
+                for step in range(len(match))
+            ]
+        )
+    except IndexError:
+        return False
+
+
+print(
+    sum(
+        [
+            get_string(lines, (x, y), kernel, "XMAS")
+            for x in range(len(lines[0]))
+            for y in range(len(lines))
+            for kernel in kernels
+        ]
+    )
+)
---
+++ 2024/04/run2.py
@@ -0,0 +1,63 @@
+import itertools
+
+with open("input") as file:
+    lines = [line.strip() for line in file.readlines()]
+
+splines = [
+    line.strip()
+    for line in """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX""".split()
+]
+
+
+kernels = [(-1, -1), (1, 1)]
+
+match = "MAS"
+
+
+def get_string(lines, pos, kernel, match):
+    try:
+        xend = pos[0] + kernel[0] * (len(match) - 1)
+        yend = pos[1] + kernel[1] * (len(match) - 1)
+        if (
+            min(xend, yend, *pos) < 0
+            or max(xend, pos[0]) > len(lines[0])
+            or max(yend, pos[1]) > len(lines)
+        ):
+            return False
+        return match == "".join(
+            [
+                lines[pos[1] + (step * kernel[1])][pos[0] + (step * kernel[0])]
+                for step in range(len(match))
+            ]
+        )
+    except IndexError:
+        return False
+
+
+print(
+    sum(
+        [
+            get_string(lines, (x, y), kernel, match)
+            and (
+                get_string(
+                    lines, (x, y + (2 * kernel[1])), (kernel[0], -kernel[1]), match
+                )
+                or get_string(
+                    lines, (x + (2 * kernel[0]), y), (-kernel[0], kernel[1]), match
+                )
+            )
+            for x in range(len(lines[0]))
+            for y in range(len(lines))
+            for kernel in kernels
+        ]
+    )
+)
