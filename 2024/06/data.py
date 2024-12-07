da58da78173ee9ebe50ac54f52eec2e17a33989c
David Rasch <rasch@raschnet.com>
2024-12-06 19:02:29 -0500

    day 6 turning

--- 2024/06/data.py
+++ 2024/06/data.py
@@ -8,6 +8,9 @@ class GridException(BaseException):
 class Grid:
     grid: list[str] = []
     width: str | None = None
+    pos: tuple[int, int]
+    direction: int
+    directions = [(-1, 0), (0, 1), (-1, 0), (0, -1)]
 
     def __init__(self, data_iter):
         for line in data_iter:
@@ -30,10 +33,19 @@ class Grid:
 
         col_pos = self.grid[row_pos].index("^")
         self.pos = (row_pos, col_pos)
+        self.direction = 0
 
     @property
     def height(self):
         return len(self.grid)
+
+    def turn_right(self):
+        self.direction = (self.direction + 1) % 4
+
+    @property
+    def move_direction(self):
+        print(self.directions)
+        return self.directions[self.direction]
 
 
 test_data = """....#.....
@@ -56,6 +68,20 @@ with open("input") as file:
 
 def test_grid_loader():
     assert grid_sample.pos == (6, 4)
+
+
+def test_turn():
+    assert grid_sample.direction == 0
+    assert grid_sample.move_direction == (-1, 0)  # starts moving up
+    grid_sample.turn_right()
+    assert grid_sample.direction == 1
+    assert grid_sample.move_direction == (0, 1)  # moving right
+    grid_sample.turn_right()
+    assert grid_sample.direction == 2
+    grid_sample.turn_right()
+    assert grid_sample.direction == 3
+    grid_sample.turn_right()  # up again?
+    assert grid_sample.direction == 0
 
 
 __all__ = ["grid", "test_grid"]
