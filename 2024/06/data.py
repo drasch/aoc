7e44570ed97666e139323e8e5f9d836e9e5d0b95
David Rasch <rasch@raschnet.com>
2024-12-06 21:15:47 -0500

    day 6 - progress

--- 2024/06/data.py
+++ 2024/06/data.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, Counter, deque
 
 
 class GridException(BaseException):
@@ -12,9 +12,11 @@ class Grid:
     pos: tuple[int, int]
     direction: int
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    turns: list[tuple[int, int]]
 
     def __init__(self, data_iter):
         self.grid = []
+        self.turns = []
         for line in data_iter:
             line = line.strip()
             if not line or not len(line):
@@ -74,26 +76,29 @@ class Grid:
     def in_bounds(self):
         return self.pos_in_bounds(self.pos)
 
-    def run(self):
-        moves = set()
-        while self.pos_in_bounds(self.pos):
-            print(self.pos)
-            moves.add(self.pos)
-            while self.blocked:
-                self.turn_right()
-            self.move()
-        print(moves)
-        return len(moves)
+    def run(self, limit=-1):
+        self.moves = defaultdict(int)
+        last = deque()
+        while self.pos_in_bounds(self.pos) and (limit < 0 or limit > 0):
+            self.moves[self.pos] += 1
 
-    def run_check_loop(self, steps=5000):
-        moves = set()
-        while self.pos_in_bounds(self.pos) and steps != 0:
-            steps -= 1
-            moves.add(self.pos)
+            if len(last) > 2:
+                last.popleft()
+            last.append(self.moves[self.pos])
+            if self.moves[self.pos] > 3 or len(set(last)) == 1 and last[0] > 3:
+                print("breaking")
+                return -1
+
+            if self.blocked:
+                self.turns.append(self.pos)
             while self.blocked:
                 self.turn_right()
             self.move()
-        return steps == 0
+            limit -= 1
+
+        if limit == 0:
+            return -1
+        return len(self.moves)
 
     def block(self, pos):
         self.grid[pos[0]] = (
@@ -159,6 +164,12 @@ def test_out():
     assert not grid_sample.in_bounds
 
 
+def test_cycle():
+    grid_sample = Grid(grid_data_test)
+    grid_sample.block((6, 3))
+    assert grid_sample.run() == -1
+
+
 def test_blocked():
     grid_sample = Grid(grid_data_test)
     assert not grid_sample.blocked
@@ -182,18 +193,85 @@ def file_grid():
 grid_sample = Grid(grid_data_test)
 
 
-def run_block_scenarios():
+def sliding_window(iterable, window_size):
+    """
+    Generates a sliding window iterator over the given iterable.
+    """
+    for i in range(len(iterable) - window_size + 1):
+        yield iterable[i : i + window_size]
+
+
+def cmp(a, b):
+    return (a > b) - (a < b)
+
+
+def not_run_block_scenarios(turns):
+    ct = 0
+    for steps in sliding_window(turns, 3):
+        ys = Counter([pos[0] for pos in steps])
+        xs = Counter([pos[1] for pos in steps])
+        print(steps)
+        print(xs)
+        print(ys)
+
+        if len(xs) == 2 and len(ys) == 2:
+            # complete the square!
+            for key in xs.keys():
+                if xs[key] == 1:
+                    x = key
+                    break
+            for key in ys.keys():
+                if ys[key] == 1:
+                    y = key
+                    break
+            print(x)
+            print(y)
+            block = (y + cmp(y, steps[2][0]), x + cmp(x, steps[2][1]))
+            grid = file_grid()
+            if grid.grid[block[0]][block[1]] != ".":
+                continue
+            grid.block(block)
+            if grid.run_check_loop():
+                ct += 1
+                break
+    return ct
+
+
+def brute_run_block_scenarios():
     grid = file_grid()
     ct = 0
     for y in range(grid.height):
         for x in range(grid.width):
             grid = file_grid()
             if grid.grid[y][x] != ".":
-                pass
+                continue
             grid.block((y, x))
             if grid.run_check_loop():
                 ct += 1
-        print(ct)
+        print(f"ct: {ct}")
+    return ct
+
+
+def run_block_scenarios(moves):
+    grid_orig = file_grid()
+    moves = set(moves.keys())
+
+    ct = 0
+    for spot in list(moves):
+        for y, x in [
+            (spot[0] + d[0], spot[1] + d[1])
+            for d in grid_orig.directions
+            if (spot[0] + d[0], spot[1] + d[1]) not in moves
+        ]:
+            moves.add((y, x))
+            grid = file_grid()
+            if grid.grid[y][x] != ".":
+                continue
+            grid.block((y, x))
+            if grid.run(10000) == -1:
+                print(f"ct: {ct}")
+                ct += 1
+    print(f"ct: {ct}")
     return ct
 
 
@@ -201,5 +279,7 @@ __all__ = ["grid", "test_grid"]
 
 if __name__ == "__main__":
     print(grid_sample.run())
-    print(file_grid().run())
-    print(run_block_scenarios())
+    grid = file_grid()
+    print(grid.run())
+    moves = grid.moves
+    print(run_block_scenarios(moves))
